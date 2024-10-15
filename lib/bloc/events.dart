d79abbe6ae832614f17e95fc9ad93c9a293cb85b
Aliza Aziz <83037489+Aliza02@users.noreply.github.com>
2024-10-15 14:02:50 +0500

    Update events.dart

--- lib/bloc/events.dart
+++ lib/bloc/events.dart
@@ -3,14 +3,5 @@ enum PaymentStatus { initial, loading, success, error }
 abstract class PaymentEvent {}
 
 class ProcessPayment extends PaymentEvent {
-  // final String amount;
-  // final String currency;
-  // final String paymentMethod;
-  // final String message;
-  ProcessPayment(
-    // required this.amount,
-    // required this.currency,
-    // required this.message,
-    // required this.paymentMethod,
-  );
+  ProcessPayment();
 }
